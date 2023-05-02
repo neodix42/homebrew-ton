5001dd9a66768146c591ba671c9954766574bc16
neodiX42 <namlem@gmail.com>
2023-05-03 01:04:42 +0300

    Create ton.rb

---
+++ ton.rb
@@ -0,0 +1,16 @@
+class Ton < Formula
+  desc "A collection of The Open Network core software and utilities."
+  homepage "http://github.com/ton-blockchain/ton"
+
+  if Hardware::CPU.arm?  
+    head "https://github.com/neodiX42/ton/releases/latest/download/ton-arm64.tar.gz"
+  else
+    head "https://github.com/neodiX42/ton/releases/latest/download/ton-am64.tar.gz"
+  end
+  license "LGPL-2.0-only"
+
+  def install
+    bin.install Dir["bin/*"]
+    lib.install Dir["lib/*"]
+  end
+end
